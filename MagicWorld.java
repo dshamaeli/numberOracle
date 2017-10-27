fed33e84d36957f8845a544681bd982ee3caecad
Daniel Shamaeli <d_sh80@yahoo.com>
2017-10-27 13:00:57 +0100

    add comments

--- MagicWorld.java
+++ MagicWorld.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.util.Scanner; //import scanner for user input
 
 class MagicWorld {
 
-    private static int var;
-    private static Scanner input = new Scanner(System.in);
-    private static NumberOracle oracle = new NumberOracle();
+    private static int num; //our test number
+    private static Scanner input = new Scanner(System.in); //scans input
+    private static NumberOracle oracle = new NumberOracle(); //our oracle
 
     public static void main(String[] args) {
 
@@ -12,18 +12,23 @@ class MagicWorld {
         do {
             //prompting user for a number
             System.out.print("Please enter your number: ");
-            //check if input is an integer
+
+            //loop until input is an integer
             while (!input.hasNextInt()) {
+
                 System.out.print("your number should be integer(<1 to quit): ");
                 input.next();
             }
+
             //getting use input
-            var = input.nextInt();
-            if (var < 1)
-                break;
-            oracle.setNum(var);    
-            oracle.checkInt();
-        } while (var > 0); //infinit loop until gets a positive number
-        System.out.println("\n\nThe Oracle is closing for business now");
+            num = input.nextInt();
+
+            //
+            if (num < 1) {
+
+                System.out.println("\n\nThe Oracle is closing for business now");
+            }
+            oracle.consult(num);
+        } while (num > 0); //infinit loop until gets a number less than 1
     }
 }
--- NumberOracle.java
+++ NumberOracle.java
@@ -2,32 +2,32 @@
 import java.util.Scanner;
 
 class NumberOracle {
-    private int num = 0;
-    private static int counter = 0;
 
-    public void setNum(int num) {
-        this.num = num;
-    }
+    private static int sum = 0;
 
-    public void checkInt() {
-        counter=0;
-        System.out.println("\nConsulting the Oracle …\n");
-        if (num == 1)
-            System.out.println("\nThe number " + num + " is deficient!\n");
-        else {
-            for (int i = 1; i < num; i++) {
-                if (num % i == 0) {
-                    counter += i;
-                }
+    //checks if num is deficient,abundant or perfect
+    public void consult(int num) {
+        sum = 0;
+        //divid num to all smaller number 
+        for (int i = 1; i < num; i++) {
+
+            //check if i proper divisors for num
+            if (num % i == 0) {
+                sum += i; // if it is, add it ti sum
             }
-            if (num > counter) {
-                System.out.println(
-                        "\nThe number " + num + " is deficient!\n\nIt has a deficiency of " + (num - counter) + "\n");
-            } else if (num < counter) {
-                System.out.println(
-                        "\nThe number " + num + " is abundant!\n\nIt has a abundancy of " + (counter - num) + "\n");
+        }    
+            //checks  if correct prints the deficiency
+            if (num == 1 || num > sum) {
+
+                System.out.println("\nThe number " + num + " is deficient!\n\nIt has a deficiency of " + (num - sum) + "\n");
+
+                //checks if num is abundant and prints the abundancy
+            } else if (num < sum) {
+
+                System.out.println("\nThe number " + num + " is abundant!\n\nIt has a abundancy of " + (sum - num) + "\n");
+
+            /*if num is not > or < than sum then they must be == so there is no need to check for perfect condition here*/
             } else
                 System.out.println("\nYou have achieved perfection with the number " + num + "!\n");
-        }
     }
 }
